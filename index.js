ee20a26c93e030c372e0365224c54c32fc4cc293
karanpratap <karanpratapsinghgem@gmail.com>
2020-05-29 18:44:28 +0530

    Model integrated with node

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const {spawn, spawnSync} = require('child_process');
+const app = express();
+const port = 3000;
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+app.post('/', (req, res) => { 
+	var dataToSend;
+	// spawn new child process to call the python script
+	if(!req.body.news)
+		return res.json(0);
+	const op = spawnSync('python3', ['./prediction.py', req.body.news], {encoding: "utf8"});
+	console.log(op.output);
+	dataToSend = op.stdout.split(' ');
+	dataToSend = parseFloat(dataToSend[dataToSend.length-3]);
+	res.json(dataToSend);
+})
+app.listen(port, () => console.log("App listening on port ${port}!"))
