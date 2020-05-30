6a5db260a3dc595cba482d06fec54016d16e41bd
karanpratap <karanpratapsinghgem@gmail.com>
2020-05-30 09:34:05 +0530

    Experimentation 1.2

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const {spawn, spawnSync} = require('child_process');
 const app = express();
-const port = 3000;
+const port = 8080;
 //const bodyParser = require('body-parser');
 
 //app.use(bodyParser.json());
